f56da7dc355a741cb88d5ac671b9898ac072e273
vperezlopez <vperezlopez.valencia@gmail.com>
2024-06-03 20:09:49 +0200

    Refactoring of a big chunk of the logic
    The left click handler could use some light refactoring. Also, there is something that shouldn't be working, yet it is, and I can't tell why (check line 274)

--- Nodes/map.gd
+++ Nodes/map.gd
@@ -11,6 +11,7 @@ var harbor = preload("res://Nodes/harbor.tscn")
 const priority_queue_class = preload("res://Nodes/PriorityQueue.gd")
 
 enum PATH {ROAD = 1, RAILWAY = 2}
+enum BuildMode { NONE, BUILDING, DEMOLISH, ROAD, DEMOLISH_ROAD }
 
 var map_width = 0
 var map_height = 0
@@ -19,6 +20,9 @@ var occupied_tiles : Dictionary = {}
 
 var mouse_tile : Vector2i
 var mouse_tile_prev : Vector2i
+
+var b_mode : BuildMode = BuildMode.NONE
+
 var selected_building : Actor_Static
 
 const HUE_GREEN_T = '#00ff007f'
@@ -26,13 +30,13 @@ const HUE_RED_T = '#ff00007f'
 const HUE_RED = '#ff0000ff'
 const HUE_DEFAULT = '#ffffffff'
 
-var build_road : bool = false
+#var build_road : bool = false
 var road_start : Vector2i
 var road_end: Vector2i
 var is_dragging : bool = false
 
-var demolish : bool = false
-var demolish_road : bool = false
+#var demolish : bool = false
+#var demolish_road : bool = false
 
 var debug_enabled : bool = false
 
@@ -62,28 +66,30 @@ func _process(_delta):
 	
 	mouse_tile_prev = mouse_tile
 	mouse_tile = local_to_map(get_global_mouse_position())
-	if selected_building:
-		selected_building.position = map_to_local(mouse_tile)
-		if valid_position(floor(selected_building.position.x), floor(selected_building.position.y)):
-			selected_building.sprite.self_modulate = HUE_GREEN_T
-		else:
-			selected_building.sprite.self_modulate = HUE_RED_T
-	
-	if build_road:
-		clear_layer(2)
-		if is_dragging:
-			road_end = local_to_map(get_global_mouse_position())
-			var path = a_star_search(road_start, road_end)
-			set_cells_terrain_connect(2, path, 0, 0)
-		else:
-			set_cells_terrain_connect(2, [mouse_tile], 0, 0)
-	
-	if demolish:
-		if occupied_tiles.has(mouse_tile_prev):
-			occupied_tiles.get(mouse_tile_prev).sprite.self_modulate = HUE_DEFAULT
-		if occupied_tiles.has(mouse_tile):
-			occupied_tiles.get(mouse_tile).sprite.self_modulate = HUE_RED
+	if b_mode != BuildMode.NONE:
+		match b_mode:
+			BuildMode.BUILDING:
+				if selected_building:
+					selected_building.position = map_to_local(mouse_tile)
+					if valid_position(floor(selected_building.position.x), floor(selected_building.position.y)):
+						selected_building.sprite.self_modulate = HUE_GREEN_T
+					else:
+						selected_building.sprite.self_modulate = HUE_RED_T
+						
+			BuildMode.ROAD:
+				clear_layer(2)
+				if is_dragging:
+					road_end = local_to_map(get_global_mouse_position())
+					var path = a_star_search(road_start, road_end)
+					set_cells_terrain_connect(2, path, 0, 0)
+				else:
+					set_cells_terrain_connect(2, [mouse_tile], 0, 0)
 			
+			BuildMode.DEMOLISH:
+				if occupied_tiles.has(mouse_tile_prev):
+					occupied_tiles.get(mouse_tile_prev).sprite.self_modulate = HUE_DEFAULT
+				if occupied_tiles.has(mouse_tile):
+					occupied_tiles.get(mouse_tile).sprite.self_modulate = HUE_RED
 
 # GENERATION FUNCTIONS
 
@@ -125,18 +131,17 @@ func draw_roads(path):
 
 # BUILDING FUNCTIONS
 
-func building_mode(actor_static_instance : Actor_Static):
-	cancel_building_mode_road()
+func preview_building(actor_static_instance : Actor_Static):
 	if selected_building:
 		remove_child(selected_building)
 	selected_building = actor_static_instance
 	add_child(selected_building)
 	selected_building.z_index = 3
 
-func cancel_building_mode():
-	if selected_building:
-		remove_child(selected_building)
-	selected_building = null
+#func cancel_building_mode():
+	#if selected_building:
+		#remove_child(selected_building)
+	#selected_building = null
 
 func build_actor_static(actor_static_instance : Actor_Static) -> bool:
 	var pos = local_to_map(get_global_mouse_position())
@@ -145,33 +150,63 @@ func build_actor_static(actor_static_instance : Actor_Static) -> bool:
 		place_actor_static(actor_static_instance, pos)
 	return valid
 
-func building_mode_road():
-	build_road = true
+#func building_mode_road():
+	#b_mode = BuildMode.ROAD
 
-func cancel_building_mode_road():
-	clear_layer(2)
-	is_dragging = false
-	build_road = false
+#func cancel_building_mode_road():
+	#clear_layer(2)
+	#is_dragging = false
+	#b_mode = BuildMode.NONE
 
-func demolish_mode():
-	demolish = true
+#func demolish_mode():
+	#b_mode = BuildMode.DEMOLISH
 
-func cancel_demolish_mode():
-	demolish = false
-	if occupied_tiles.has(mouse_tile_prev):
-		occupied_tiles.get(mouse_tile_prev).sprite.self_modulate = HUE_DEFAULT
-	if occupied_tiles.has(mouse_tile):
-		occupied_tiles.get(mouse_tile).sprite.self_modulate = HUE_DEFAULT
+#func cancel_demolish_mode():
+	#b_mode = BuildMode.NONE
+	##if occupied_tiles.has(mouse_tile_prev):
+		##occupied_tiles.get(mouse_tile_prev).sprite.self_modulate = HUE_DEFAULT
+	#if occupied_tiles.has(mouse_tile):
+		#occupied_tiles.get(mouse_tile).sprite.self_modulate = HUE_DEFAULT
 
-func demolish_road_mode():
-	demolish_road = true
+#func demolish_road_mode():
+	#b_mode = BuildMode.DEMOLISH_ROAD
 
-func cancel_demolish_road_mode():
-	demolish_road = false
+#func cancel_demolish_road_mode():
+	#b_mode = BuildMode.NONE
 
-func demolish_road_fun():
-	set_cell(1, mouse_tile, -1)
-	pass
+func set_build_mode(bm : BuildMode) :
+	
+	# REMOVE PREVIOUS MODE
+	match b_mode:
+		BuildMode.NONE:
+			pass
+		BuildMode.BUILDING:
+			if selected_building:
+				remove_child(selected_building)
+			selected_building = null
+		BuildMode.ROAD:
+			is_dragging = false
+			clear_layer(2)
+		BuildMode.DEMOLISH:
+			if occupied_tiles.has(mouse_tile):
+				occupied_tiles.get(mouse_tile).sprite.self_modulate = HUE_DEFAULT
+		BuildMode.DEMOLISH_ROAD:
+			is_dragging = false
+	
+	#SET NEW BUILD MODE
+	match bm:
+		BuildMode.NONE:
+			pass
+		BuildMode.BUILDING:
+			pass
+		BuildMode.ROAD:
+			pass
+		BuildMode.DEMOLISH:
+			pass
+		BuildMode.DEMOLISH_ROAD:
+			pass
+	
+	b_mode = bm
 
 # UTILITIES
 
@@ -196,65 +231,89 @@ func _input(event):
 	if event is InputEventKey:
 		if event.pressed:
 			if event.keycode == KEY_ESCAPE:
-				get_tree().quit()
+				if b_mode != BuildMode.NONE:
+					set_build_mode(BuildMode.NONE)
+				else:
+					get_tree().quit()
 			if event.keycode == KEY_K:
 				debug_enabled = !debug_enabled
 				print("Debug mode: " + ['disabled', 'enabled'][int(debug_enabled)])
-			if event.keycode == KEY_F:
-				building_mode(factory.instantiate())
-			if event.keycode == KEY_D:
-				building_mode(depot.instantiate())
-			if event.keycode == KEY_W:
-				building_mode(warehouse.instantiate())
 			if event.keycode == KEY_C:
 				for child in get_children():
-					print(child.get_class)
+					if child is Actor_Static:
+						print(child.get_class)
+			if event.keycode == KEY_O:
+				for tile in occupied_tiles:
+					print('Structure: ' + str(occupied_tiles.get(tile)) + ', Position: ' + str(tile))
+			if event.keycode == KEY_M:
+				print('Building Mode: ' + str(b_mode))
+			if event.keycode == KEY_F:
+				set_build_mode(BuildMode.BUILDING)
+				preview_building(factory.instantiate())
+			if event.keycode == KEY_D:
+				set_build_mode(BuildMode.BUILDING)
+				preview_building(depot.instantiate())
+			if event.keycode == KEY_W:
+				set_build_mode(BuildMode.BUILDING)
+				preview_building(warehouse.instantiate())
 			if event.keycode == KEY_R:
-				cancel_building_mode()
-				clear_layer(2)
-				build_road = !build_road
+				set_build_mode(BuildMode.ROAD)
 			if event.keycode == KEY_X:
-				if !demolish:
-					demolish_mode()
-				else:
-					cancel_demolish_mode()
+				set_build_mode(BuildMode.DEMOLISH)
 			if event.keycode == KEY_Z:
-				if !demolish:
-					demolish_road_mode()
-				else:
-					cancel_demolish_road_mode()
+				set_build_mode(BuildMode.DEMOLISH_ROAD)
 				
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
-				if selected_building:
-					selected_building.sprite.self_modulate = '#ffffffff'
-					var placed = build_actor_static(selected_building)
-					if !placed:
-						cancel_building_mode()
-					selected_building = null
-				if build_road:
-					road_start = local_to_map(get_global_mouse_position())
-					is_dragging = true
-					#set_cells_terrain_connect(1, [mouse_tile], 0, 0)
-				if demolish:
-					if occupied_tiles.has(mouse_tile):
-						remove_child(occupied_tiles.get(mouse_tile))
-				if demolish_road:
-					demolish_road_fun()
-					demolish_road = false
+				match b_mode:
+					BuildMode.BUILDING:
+						if selected_building:
+							selected_building.sprite.self_modulate = HUE_DEFAULT
+							var placed = build_actor_static(selected_building)
+							if !placed: # CHECK WHY THIS LOGIC WORKS
+								#cancel_building_mode()
+								set_build_mode(BuildMode.NONE)
+							selected_building = null
+					BuildMode.ROAD:
+						road_start = local_to_map(get_global_mouse_position())
+						is_dragging = true
+					BuildMode.DEMOLISH:
+						if occupied_tiles.has(mouse_tile):
+							remove_child(occupied_tiles.get(mouse_tile))
+					BuildMode.DEMOLISH_ROAD:
+						set_cells_terrain_connect(1, [mouse_tile], -1, -1)
 			else:
-				if build_road:
-					road_end = local_to_map(get_global_mouse_position())
-					var path = a_star_search(road_start, road_end)
-					set_cells_terrain_connect(1, path, 0, 0)
-					cancel_building_mode_road()
+				match b_mode:
+					BuildMode.ROAD:
+						road_end = local_to_map(get_global_mouse_position())
+						var path = a_star_search(road_start, road_end)
+						set_cells_terrain_connect(1, path, 0, 0)
+						is_dragging = false
 
 		if event.button_index == MOUSE_BUTTON_RIGHT:
-			cancel_building_mode_road()
-			if selected_building:
-				cancel_building_mode()
-			cancel_demolish_mode()
+			set_build_mode(BuildMode.NONE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # A* Algorithm PENDING IMPROVEMENTS TO GENERATE STRAIGHT ROADS MORE OFTEN
